7fadb59c858f9cf7011ba0ba94a1ce42a94e626c
schrodingerzhu <i@zhuyi.fan>
2019-12-08 23:26:30 +0800

    update the bad hello.h

--- src/hello.h
+++ src/hello.h
@@ -1,4 +1,9 @@
 // This header file doesn't do much, but it's good to demonstrate that it's
 // possible to create and edit header files
+// one thing to notice that uyou should carefully avoid redefinitions
+#ifndef HELLO_H
+#define HELLO_H
 
 #include <iostream>
+
+#endif //HELLO_H
