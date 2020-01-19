db8b2d5dca3501cdefe55771de13eb6d9bb28a53
schrodingerzhu <i@zhuyi.fan>
2020-01-19 18:26:42 +0800

    fine, fine, fine, windows must be kidding me

--- misc/test-nox.sh
+++ misc/test-nox.sh
@@ -1,3 +1,7 @@
 # shellcheck disable=SC2010
 set -e
-for i in $(ls -l | grep -v "^d" | grep test_ | rev | cut -d" " -f1 | rev); do ./$i; done;
+./test_locks
+./test_queue
+./test_strlib
+./test_vector
+
